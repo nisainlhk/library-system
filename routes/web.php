4b3cec344c012669b058949ff059d6b0220a2bf3
nisainlhk <nisaainulhakim@gmail.com>
2026-09-18 10:22:35 +0700

    Add route parameter

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,14 @@ Route::get('/', function () {
   //  return view('books.index');
 //});
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/members', [MemberController::class, 'index']);
+Route::get('/books', [BookController::class, 'index'])->name('buku');
+
+Route::get('/books/{id}', [BookController::class, 'show']);
+
+Route::get('/categories', [CategoryController::class, 'index'])->name('kategori');
+Route::get('/members', [MemberController::class, 'index'])->name('anggota');
+Route::get('/members/{id}', [MemberController::class, 'show'])->name('anggota.show');
+
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+
+?>
