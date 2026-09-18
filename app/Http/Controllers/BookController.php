b081b0d7bac92115bb485a005dd5ea74ec2af004
nisainlhk <nisaainulhakim@gmail.com>
2026-09-18 10:21:27 +0700

    Add books pages

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,8 +6,29 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function index()
+    public function index(){
+       $title = 'Daftar Buku';
+       $description = 'Koleksi buku yang tersedia di perpustakaan';
+       $books = [
+        'Hujan by Tere Liye (2016)',
+        'Filosofi Teras by Henry Manampiring (2018)',
+        'Becoming by Michelle Obama (2018)',
+        'Homo Deus: A Brief History of Tomorrow by Yuval Noah Harari  (2015)',
+        'Brief Answers to the Big Questions by Stephen Hawking (2018)',
+        'Life 3.0: Being Human in the Age of Artificial Intelligence by Max Tegmark (2017)',
+        'Seni Merayu Tuhan by M. Fauzi Hidayat (2022)',
+        'The Psychology of Money by Morgan Housel (2020)',
+        'Sumur by Eka Kurniawan (2021)',
+        'Glucose Revolution by Jessie Inchauspe (2022)',
+      ];
+
+       $stock = 10;
+
+        return view('books.index', compact('title', 'description', 'books', 'stock'));
+    }
+
+    public function show($id)
     {
-        return view('books.index');
+        return view ('books.show', compact('id'));
     }
 }
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,2 +1,22 @@
-<h1>Daftar Buku</h1>
-<p>Sistem Informasi Perpustakaan</p>
+@extends('layouts.app')
+
+@section('title', $title)
+
+@section('content')
+    <h1>{{ $title }}</h1>
+    <p>{{ $description }}</p>
+
+    <ul>   
+        @foreach ($books as $book)
+            <li>{{ $book }}</li>
+        @endforeach
+    </ul>
+
+    <!-- @if ($stock > 0)
+        <p>Stok tersedia</p>
+    @else
+        <p>Stok habis</p>
+    @endif -->
+
+@endsection
+    
